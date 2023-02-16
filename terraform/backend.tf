6e815a1845765a05c1f8cc8d7e95668cbe9d4474
Jean-BaptisteL <jean-baptiste.lagand@laposte.net>
2023-02-16 17:18:09 +0100

    infras

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,4 +1,9 @@
 terraform {
+    backend "s3" {
+    bucket = "cdap-jb-beaura"
+    key    = "terraform.tfstate"
+    region = "eu-west-2"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -7,16 +12,6 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
-  region = "eu-west-2"
-}
-
-data "terraform_remote_state" "cdap-jb-beaura" {
-  backend = "s3"
-  config = {
-    bucket = "cdap-jb-beaura"
-    key    = "terraform.tfstate"
-    region = "eu-west-2"
-  }
+  region  = "eu-west-2"
 }
