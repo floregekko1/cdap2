bf785d7b27cc03c6036f3a672d29a4fdadb76d79
yve-n <yve.nguetche@gmail.com>
2023-02-16 16:50:38 +0100

    create subnet public and private

---
+++ terraform/subnet.tf
@@ -0,0 +1,25 @@
+#create a public subnet
+resource "aws_subnet" "subnet-cdap-public" {
+  vpc_id     = aws_vpc.cdap2-vpc.id
+  cidr_block = "172.198.0.0/28"
+
+ tags = {
+    Name    = "cdap2-vpc"
+    Project = "infrastructure"
+    Teams   = "modibo-yve"
+    Manage  = "terraform"
+  }
+}
+
+#create a private subnet
+resource "aws_subnet" "subnet-cdap-private" {
+  vpc_id     = aws_vpc.cdap2-vpc.id
+  cidr_block = "172.198.0.16/28"
+
+  tags = {
+    Name    = "cdap2-vpc"
+    Project = "infrastructure"
+    Teams   = "modibo-yve"
+    Manage  = "terraform"
+  }
+}
