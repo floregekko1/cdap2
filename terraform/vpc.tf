5f77a28b5ab89ea5ec010f9d0ec5f29e1f1246e5
beaura-ngo <ngo.beaura@gmail.com>
2023-02-16 17:06:20 +0100

    subnet creation

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -8,3 +8,29 @@ resource "aws_vpc" "cdap2-vpc" {
     Manage  = "terraform"
   }
 }
+
+# Subnet public
+resource "aws_subnet" "subnet-cdap-public" {
+  vpc_id     = aws_vpc.cdap2-vpc.id
+  cidr_block = "172.198.0.16/28"
+
+  tags = {
+    Name    = "subnet-cdap2-public"
+    Project = "infrastructure"
+    Teams   = "JB-beaura"
+    Manage  = "terraform"
+  }
+}
+
+# Subnet private
+resource "aws_subnet" "subnet-cdap-private" {
+  vpc_id     = aws_vpc.cdap2-vpc.id
+  cidr_block = "172.198.0.0/28"
+
+  tags = {
+    Name    = "subnet-cdap2-private"
+    Project = "infrastructure"
+    Teams   = "JB-beaura"
+    Manage  = "terraform"
+  }
+}
