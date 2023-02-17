5ee8736caf48eeb515dc5d3edc9f8e6e9489d3db
yve-n <yve.nguetche@gmail.com>
2023-02-17 16:13:08 +0100

    update security group

--- terraform/security-group.tf
+++ terraform/security-group.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "cdap2-security-group" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.cdap2-vpc.cidr_block]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   egress {
