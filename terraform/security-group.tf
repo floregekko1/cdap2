80bb6d9d0d1490540f5a712accadc10b435a38bb
yve-n <yve.nguetche@gmail.com>
2023-02-17 14:24:16 +0100

    add security group

---
+++ terraform/security-group.tf
@@ -0,0 +1,27 @@
+resource "aws_security_group" "cdap2-security-group" {
+  name        = "cdap2-security-group"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.cdap2-vpc.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.cdap2-vpc.cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "cdap2-security-group"
+    Project = "infrastructure"
+    Teams   = "modibo-yve"
+    Manage  = "terraform"
+  }
+}
